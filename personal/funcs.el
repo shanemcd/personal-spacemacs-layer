8bfe0f2e0c4ec3e7a331d679cf17bb519e4f7857
Shane McDonald <Shane@Shanes-MacBook.local>
2015-05-31 20:38:50 -0400

    Remove more tag stuff

--- personal/funcs.el
+++ personal/funcs.el
@@ -1,10 +1,3 @@
-(defadvice find-tag (before c-tag-file activate)
-  "Automatically create tags file."
-  (let ((tag-file (concat default-directory "TAGS")))
-    (unless (file-exists-p tag-file)
-      (shell-command "ctags -e -a --Ruby-kinds=-fF -o TAGS -R"))
-    (visit-tags-table tag-file)))
-
 (defun seeing-is-believing ()
   "Replace the current region (or the whole buffer, if none) with the output of seeing_is_believing."
   (interactive)
@@ -16,12 +9,6 @@
 
 (global-set-key "\M-." 'helm-etags-select)
 
-(defadvice helm-etags-select (before c-tag-file activate)
-   "Automatically create tags file."
-   (let ((tag-file (concat default-directory "TAGS")))
-     (unless (file-exists-p tag-file)
-       (shell-command "ripper-tags -R -f TAGS"))
-     (visit-tags-table tag-file)))
-
 (global-set-key (kbd "M-x") 'helm-M-x)
 
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
